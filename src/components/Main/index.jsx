a999f5a2fcaa9fc281ec2347a159ec93082e0445
Samuel Ramos Barroso <alu0100815146@ull.edu.es>
2018-07-29 13:25:29 +0100

    adding messages

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -1,11 +1,29 @@
 import React, { Component } from 'react'
 
+import MessageList from '../MessageList'
+
 class Main extends Component {
+  constructor () {
+    super()
+    this.state = {
+      messages: [{
+        text: 'Mensaje del Tweet',
+        picture: 'https://pbs.twimg.com/profile_images/1005086461232910336/duCR-EIz_bigger.jpg',
+        displayName: 'Samuel Ramos',
+        username: 'Losnen',
+        date: Date.now() - 180000
+      }, {
+        text: 'Este es un nuevo mensaje',
+        picture: 'https://pbs.twimg.com/profile_images/1005086461232910336/duCR-EIz_bigger.jpg',
+        displayName: 'Samuel Ramos',
+        username: 'Losnen',
+        date: Date.now() - 1800000
+      }]
+    }
+  }
   render () {
     return (
-      <div>
-        Main
-      </div>
+      <MessageList messages={this.state.messages} />
     )
   }
 }
