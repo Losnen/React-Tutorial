6aa9958579c6555ec4c9ef2f6e98b1a74937f095
Samuel Ramos Barroso <alu0100815146@ull.edu.es>
2018-08-04 11:05:50 +0100

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -5,12 +5,6 @@ import firebase from 'firebase'
 import App from './components/App'
 
 firebase.initializeApp({
-  apiKey: 'AIzaSyC_f6GO36_IvPccEuUFS0DTCvasf4mLPBg',
-  authDomain: 'curso-react-12.firebaseapp.com',
-  databaseURL: 'https://curso-react-12.firebaseio.com',
-  projectId: 'curso-react-12',
-  storageBucket: 'curso-react-12.appspot.com',
-  messagingSenderId: '750293749516'
 })
 
 render(<App />, document.getElementById('root'))
